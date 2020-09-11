af95fb5e34261b459173b22e00ee209f885b5f4c
YU HONG <70290564+Yukiooooo@users.noreply.github.com>
2020-09-11 16:41:14 -0500

    Add files via upload

---
+++ assignment-1/arrow.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 17:42:31 2020
+
+@author: Yu Hong
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+X, Y = np.meshgrid(np.arange(0, 2 * np.pi, .2), np.arange(0, 2 * np.pi, .2))
+U = np.cos(X)
+V = np.sin(Y)
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('Arrows scale with plot width, not view')
+Q = ax1.quiver(X, Y, U, V, units='width')
+qk = ax1.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+
+fig2, ax2 = plt.subplots()
+ax2.set_title("pivot='mid'; every third arrow; units='inches'")
+Q = ax2.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
+               pivot='mid', units='inches')
+qk = ax2.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+ax2.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
+
+
+plt.show()
